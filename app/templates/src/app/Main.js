77495c3ec9b67ae56958c656f58fe0c5791c19e4
Martin Vezina <m.vezina@la-grange.ca>
2015-10-21 21:48:19 -0400

    removed tests in html/js

--- app/templates/src/app/Main.js
+++ app/templates/src/app/Main.js
@@ -5,12 +5,5 @@ import $ from 'jquery';
 import ns from 'ns';
 
 ns.docReady.then(() => {
-	let f = (a, b='yo yeah') => {
-		$('#main').html('ok manne2 '+ b);
-
-		// console.log(Promise);
-		console.log(window.GreenSockGlobals);
-	};
-
-	f();
+	
 });
