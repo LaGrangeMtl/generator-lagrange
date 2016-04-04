0f87b7c3349bd30bb9f8e753618045668daa46f5
Maxime Des Roches <m.desroches@la-grange.ca>
2016-04-04 14:24:36 -0400

    promises error handling

--- app/templates/src/app/VendorShim.js
+++ app/templates/src/app/VendorShim.js
@@ -1,8 +1,14 @@
-
+import Promise from 'promise';
 
 //Puts jQuery in global scope for non-commonjs plugins
 window.$ = window.jQuery = (window.jQuery || require('jquery'));
 
 window.GreenSockGlobals = {};
+
+if(!Promise) {
+	Promise.onPossiblyUnhandledRejection(function(error){
+		throw error;
+	});
+}
 
 require('console-polyfill');
